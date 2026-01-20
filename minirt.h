2bd43fa0db654f6801a293d9fe1e5ee8e72eadb7
Ebichan610 <yuya.ebi@gmail.com>
2026-01-20 15:53:21 +0900

    test

---
+++ minirt.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebichan <ebichan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 15:46:28 by ebichan           #+#    #+#             */
+/*   Updated: 2026/01/20 15:52:14 by ebichan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+#define MINIRT_H
+
+#include "mlx.h"
+#include "libft.h"
+#include <fcntl.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/errno.h>
+
+#endif
