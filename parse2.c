d6543a65323d1c459ac40752287e97d32651fae4
Ebichan610 <yuya.ebi@gmail.com>
2026-02-18 16:39:45 +0900

    remove test_dir

--- parse2.c
+++ parse2.c
@@ -6,8 +6,23 @@
 /*   By: ebichan <ebichan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 01:47:53 by ebichan           #+#    #+#             */
-/*   Updated: 2026/02/05 01:47:58 by ebichan          ###   ########.fr       */
+/*   Updated: 2026/02/18 16:34:30 by ebichan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+bool parse_sphere(t_elem *elem, char **strs)
+{
+
+}
+
+bool parse_plane(t_elem *elem, char **strs)
+{
+
+}
+
+bool parse_cylinder(t_elem *elem, char **strs)
+{
+
+}
