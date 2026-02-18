785663da833b470c08b291c7dfc967eb8efb4466
Ebichan610 <yuya.ebi@gmail.com>
2026-02-18 15:48:47 +0900

    error check change:isinf->isnan

--- in_range.c
+++ in_range.c
@@ -6,7 +6,7 @@
 /*   By: ebichan <ebichan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 01:49:23 by ebichan           #+#    #+#             */
-/*   Updated: 2026/02/18 14:46:40 by ebichan          ###   ########.fr       */
+/*   Updated: 2026/02/18 15:46:59 by ebichan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ bool in_ratio_range(char *nptr)
     double num;
 
     num = ft_atof(nptr);
-    if(num == INFINITY)
+    if(isnan(num))
         return(false);
     if(num < 0.0 || num > 1.0)
         return(false);
@@ -31,7 +31,7 @@ bool in_color_range(char *nptr)
     if(check_positive_int_num(nptr) == false)
         return(false);
     num = (double)ft_atoi(nptr);
-    if(num == INFINITY)
+    if(isnan(num))
         return(false);
     if(num < 0.0 || num > 255.0)
         return(false);
@@ -43,7 +43,7 @@ bool in_double_range(char *nptr, double_range_mode mode)
     double num;
 
     num = ft_atof(nptr);
-    if(num == INFINITY)
+    if(isnan(num))
         return(false);
     if(mode == DOUBLE_POSITIVE && num < 0.0)
         return(false);
@@ -55,7 +55,7 @@ bool in_vector_range(char *nptr)
     double num;
 
     num = ft_atof(nptr);
-    if(num == INFINITY)
+    if(isnan(num))
         return(false);
     if(num < -1.0 || num > 1.0)
         return(false);
@@ -67,7 +67,7 @@ bool in_degree_range(char *nptr)
     double num;
 
     num = ft_atof(nptr);
-    if(num == INFINITY)
+    if(isnan(num))
         return(false);
     if(num < 0.0 || num > 180.0)
         return(false);
