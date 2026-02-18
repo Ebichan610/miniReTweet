052ac0a9250ce026ab63d986384f2baed66de5bd
Ebichan610 <yuya.ebi@gmail.com>
2026-02-18 16:32:59 +0900

    fix:struct

--- minirt.h
+++ minirt.h
@@ -6,7 +6,7 @@
 /*   By: ebichan <ebichan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 15:46:28 by ebichan           #+#    #+#             */
-/*   Updated: 2026/02/09 17:25:11 by ebichan          ###   ########.fr       */
+/*   Updated: 2026/02/18 16:29:59 by ebichan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,89 +24,105 @@
 #include <sys/errno.h>
 #include <sys/time.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 typedef enum
 {
     DOUBLE_ALL,
     DOUBLE_POSITIVE
 }   double_range_mode;
 
+typedef struct s_point
+{
+    double x;
+    double y;
+    double z;
+}   t_point;
+
+typedef struct s_vector
+{
+    double vx;
+    double vy;
+    double vz;
+}   t_vector;
+
+typedef struct s_color
+{
+    double r;
+    double g;
+    double b;
+}   t_color;
+
 typedef struct s_amb
 {
     double   ratio;
-    double   red;
-    double   green;
-    double   blue;
+    t_color color;
 }   t_amb;
 
 typedef struct s_camera
 {
-    double   x;
-    double   y;
-    double   z;
-    double   vx;
-    double   vy;
-    double   vz;
+    t_point point;
+    t_vector vector;
     double   fov;
 }   t_camera;
 
 typedef struct s_light
 {
-    double   x;
-    double   y;
-    double   z;
+    t_point point;
     double   ratio;
-    double   red;
-    double   green;
-    double   blue;
+    t_color color;
 }   t_light;
 
 typedef struct s_sphere
 {
-    double   x;
-    double   y;
-    double   z;
+    t_point point;
     double   r;
-    double   red;
-    double   green;
-    double   blue;
+    t_color color;
+    t_sphere *next;
 }   t_sphere;
 
 typedef struct s_plane
 {
-    double   x;
-    double   y;
-    double   z;
-    double   vx;
-    double   vy;
-    double   vz;
-    double   red;
-    double   green;
-    double   blue;
+    t_point point;
+    t_vector vector;
+    t_color color;
+    t_plane *next;
 }   t_plane;
 
 typedef struct s_cylinder
 {
-    double   x;
-    double   y;
-    double   z;
-    double   vx;
-    double   vy;
-    double   vz;
+    t_point point;
+    t_vector vector;
     double   r;
     double   h;
-    double   red;
-    double   green;
-    double   blue;
+    t_color color;
+    t_cylinder *cylinder;
 }   t_cylinder;
-
-typedef struct s_minirt
-{
-
-}   t_minirt;
 
 typedef struct s_elem
 {
-
+    t_amb *amb;
+    t_camera *camera;
+    t_cylinder *cylinder;
+    t_light *light;
+    t_plane *plane;
+    t_sphere *sphere;
+    bool amb_flag;
+    bool camera_flag;
+    bool light_flag;
 }   t_elem;
+
+typedef struct s_minirt
+{
+    t_elem *elem;
+}   t_minirt;
+
+char	*get_next_line(int fd);
+void	ft_bzero(void *s, size_t n);
+ssize_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
 
 #endif
